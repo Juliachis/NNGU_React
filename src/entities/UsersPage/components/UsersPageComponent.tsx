e57e00ed90dfabf23e84b9fdd42fb7588db77e3f
Juliachis <j.chistyachenko@gmail.com>
2022-06-12 15:27:05 +0700

    lesson 7 Redux

--- src/entities/UsersPage/components/UsersPageComponent.tsx
+++ src/entities/UsersPage/components/UsersPageComponent.tsx
@@ -1,6 +1,10 @@
-import React, { FC, useState } from 'react';
+import React, { FC, FormEvent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { IUsers } from '../../../interfaces/IUsers';
 import Pagination from '../../../shared/Pagination';
+import { createUserAction } from '../../../store/pages/UsersPage/actions';
+
 import style from '../UsersPage.module.scss';
 import UserCard from './UserCard';
 
@@ -12,11 +16,27 @@ const UsersPageComponent: FC<IProps> = ({ usersDataAttr }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const cardLimit = 3;
 
+  const { search } = useLocation();
+  const dispatch = useDispatch();
+  const [nameValue, setNameValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+
   const lastCardIndex = currentPage * cardLimit;
   const firstCardIndex = lastCardIndex - cardLimit;
   const currentCards = usersDataAttr.slice(firstCardIndex, lastCardIndex);
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newUser = {
+      id: usersDataAttr.length + 1,
+      name: nameValue,
+      email: emailValue,
+    };
+    dispatch(createUserAction(newUser));
+  };
 
   return (
     <div>
@@ -27,7 +47,7 @@ const UsersPageComponent: FC<IProps> = ({ usersDataAttr }) => {
             const { id, name, email } = user;
             return (
               <React.Fragment key={`UserId:${id}`}>
-                <UserCard id={id} name={name} email={email} />
+                <UserCard id={id || 0} name={name} email={email} />
               </React.Fragment>
             );
           })
@@ -35,6 +55,24 @@ const UsersPageComponent: FC<IProps> = ({ usersDataAttr }) => {
           <div>Пользователи не найдены.</div>
         )}
         <Pagination limit={3} itemsAmount={usersDataAttr.length} paginate={paginate} />
+
+        <hr />
+
+        <form className={style.create_user__form_wrapper} onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            placeholder="Введите имя"
+            value={nameValue}
+            onChange={(event) => setNameValue(event.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Введите email"
+            value={emailValue}
+            onChange={(event) => setEmailValue(event.target.value)}
+          />
+          <button type="submit">Создать пользователя</button>
+        </form>
       </div>
     </div>
   );
--- src/entities/UsersPage/index.tsx
+++ src/entities/UsersPage/index.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { usersUrl } from '../../api/constats';
 import { IUsers } from '../../interfaces/IUsers';
 import UsersPageComponent from './components/UsersPageComponent';
+import { getUsersData } from '../../store/pages/UsersPage/selectors';
+import { setUsersDataAction } from '../../store/pages/UsersPage/actions';
 
 const UsersPage = () => {
-  const [usersData, setUsersData] = useState<IUsers[] | null>(null);
+  // const [usersData, setUsersData] = useState<IUsers[] | null>(null);
+  const dispatch = useDispatch();
+  const usersData = useSelector(getUsersData);
 
   const { pathname, search } = useLocation();
   const navigate = useNavigate();
@@ -13,7 +18,8 @@ const UsersPage = () => {
   const getData = async (url: string) => {
     const response = await fetch(url);
     const data = await response.json();
-    setUsersData(data);
+    // setUsersData(data);
+    dispatch(setUsersDataAction(data));
   };
 
   useEffect(() => {
@@ -26,15 +32,13 @@ const UsersPage = () => {
   }, [pathname, navigate, search]);
 
   useEffect(() => {
-    setTimeout(() => {
-      getData(usersUrl);
-    }, 1500);
+    getData(usersUrl);
+    // dispatch({ type: 'SET_DATA', payload: [1, 2, 3] });
   }, []);
+
+  console.log(usersData);
 
   return !usersData ? <div>Загрузка...</div> : <UsersPageComponent usersDataAttr={usersData} />;
 };
 
 export default UsersPage;
-function useNavigation() {
-  throw new Error('Function not implemented.');
-}
